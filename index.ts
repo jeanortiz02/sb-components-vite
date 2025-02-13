b3059aa4c8f08a4c72900802a7e89f4a0189dc3f
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2025-02-12 22:23:11 -0400

    fini

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { MyLabel } from './src/components/MyLabel';
+export { Button } from './src/stories/Button';
+export { Header } from './src/stories/Header';
+export { Page } from './src/stories/Page';
